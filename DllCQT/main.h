50fffd3f864943ca25af21f2582511ebc96a7711
松崎好浩 <y.matsuzaki@ris.co.jp>
2022-09-20 11:07:50 +0900

    更新

--- DllCQT/main.h
+++ DllCQT/main.h
@@ -4,7 +4,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    __declspec(dllexport) void aaa();
+    __declspec(dllexport) void wavein_open(
+        int sampleRate,
+        int bits,
+        int bufferLength,
+        int bufferCount
+    );
+    __declspec(dllexport) void wavein_close();
 #ifdef __cplusplus
 }
 #endif
